79e940b1bd35e0786aa89182301960466493ec43
visualstudio <visualstudio@122-PC33>
2019-02-01 20:52:18 -0500

    village class created

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -72,5 +72,62 @@ namespace ConsoleApp2
         public bool isAstrildeHere = false;
         public Village() { }
     }
+
+
+class Program
+{
+static void Main(string[] args)
+{
+//Countryside c = new Countryside();
+//c.run();
+}
+}
+
+class LearningExperiment
+{
+public void run()
+{
+Village Toronto;
+Toronto = new Village();
+Village a, b, c;
+Toronto.villageName = "version A";
+a = Toronto;
+Console.WriteLine(a.villageName);
+Toronto = new Village();
+Toronto.villageName = "version B";
+b = Toronto;
+Console.WriteLine(b.villageName);
+Toronto = new Village();
+Toronto.villageName = "version C";
+c = Toronto;
+Console.WriteLine(c.villageName);
+
+
+}
+}
+
+
+class Village
+{
+public bool isAstrilde;
+public Village nextVillage;
+public Village prevVillage;
+public string villageName;
+}
+class Countryside
+{
+Village Toronto, Maple, Ajax;
+
+public void run()
+{
+Maple = new Village();
+Toronto = new Village();
+Ajax = new Village();
+Maple.villageName = "Toronto";
+Maple.nextVillage = Toronto;
+
+}
+}
+}
 }
 
